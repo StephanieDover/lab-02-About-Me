7380f880ffdd72f9f05e71a01d25b4968a5fa263
Stephanie Dover <doverstephaniem@gmail.com>
2017-04-05 20:48:16 -0700

    added top ten list, work, and education to html, added photo, additional styles to css, added number guessing question and city guessing question to js file and added ability to count correct answers and alert user

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 
-
 'use strict';
 
-
+var numCorrectAnswer = 1;
+var correctAnswer = false;
 var userName = prompt('What is your name?');
+var correctNumber = 8;
+var notDone = true;
+var guess;
 alert('ok ' + userName + ', lets see how well you know me');
+
+correctAnswer = false;
+var cities = ['pullman', 'bainbridge', 'bellevue', 'mukilteo']
+var cityLength = cities.length;
+for(var guesses = 9; guesses > 0 && notDone; guesses--) {
+var guessCity =  prompt('Guess a city where I previsously lived in Washington State');
+for (var cityLoop = 0; cityLoop < cityLength; cityLoop++) {
+  if(guessCity.trim().toLowerCase() == cities[cityLoop]) {
+    alert('Great Guess! ' + userName + ', You are correct!')
+    correctAnswer = true;
+    notDone = false;
+    numCorrectAnswer++;
+    console.log( userName + ' answered cities prompt correctly')
+  }
+}
+if (!correctAnswer) {
+  alert('sorry, wrong answer')
+}
+}
+
 
 var seattle = prompt('Do I live in Seattle?')
 if (seattle.trim().toLowerCase() == 'yes') {
@@ -13,39 +36,45 @@ if (seattle.trim().toLowerCase() == 'yes') {
 } else {
   alert('Correct! I live in Bothell, not Seattle');
   console.log(userName + ' Answered correctly to do I live in Seattle');
+  numCorrectAnswer++;
 };
-document.getElementById('live').style.display = 'inline-block';
+//document.getElementById('live').className = 'show';
+//document.getElementById('live').style.display = 'inline-block';
+
 
 var cats = prompt('do I like cats?')
 if (cats.trim().toLowerCase() == 'yes') {
   alert('Correct!');
   console.log(userName + ' Answered correctly to do I like cats')
+  numCorrectAnswer++;
 } else {
   alert('Sorry, not correct');
   console.log(userName + ' says I do not like cats, but cats are my favorite');
 };
-document.getElementById('meow').style.display = 'inline-block';
+//document.getElementById('meow').style.display = 'inline-block';
 
 var pizza = prompt('Do I think pineapple belongs on pizza??')
 if (pizza.trim().toLowerCase() == 'yes') {
   alert('thats right! because its delicious!');
   console.log(userName + ' answered correclty to Do I think pineapple belongs on pizza ');
+  numCorrectAnswer++;
 } else {
   alert('sorry, I lOVE pineapple on my pizza');
   console.log(userName + ' does not think I like pineapple on my pizza');
 };
-document.getElementById('pineapple').style.display = 'inline-block';
+//document.getElementById('pineapple').style.display = 'inline-block';
 
 
 var pink = prompt('Is my hair pink?')
 if (pink.trim().toLowerCase() == 'yes') {
   alert('correct!');
   console.log(userName + ' Answered correctly to is hair pink')
+  numCorrectAnswer++;
 } else {
   alert("hmm, you probably think it's #FF3DA3");
   console.log(userName + ' thinks hair is #FF3DA3');
 };
-document.getElementById('hair').style.display = 'inline-block';
+//document.getElementById('hair').style.display = 'inline-block';
 
 
 var harryPotter = prompt('Am I in Slytherin?')
@@ -55,52 +84,31 @@ if (harryPotter.trim().toLowerCase() == 'yes') {
 } else {
   alert('Correct! Gryffindor is the right house!');
   console.log(userName + ' Answered correctly to am I in Slytherin');
+  numCorrectAnswer++;
 };
 document.getElementById('pottermore').style.display = 'inline-block';
 
 
-alert('Thanks for playing ' + userName + ', now you are my new bff');
 
-//end comment our finished code
-
-
-
-
-
-
-
-/*
-
-for(setup; condition; end of loop code){
-
-}
-
-for (var i=0; i<10; i++) {
-console.log(i)
-}
-
-cat = [12, 21, 33, 55];
-for (var i = 0; i < cat.length; i++){
-console.log(cat[i]);
+for(var guesses = 9; guesses > 0 && notDone; guesses--) {
+  guess = prompt('what is my favorite number?');
+  if (guess == correctNumber) {
+    notDone = false;
+    alert('You got it correct!');
+    numCorrectAnswer++;
+  } else if (guesses === 0) {
+    alert('sorry, you are out of guesses! Reload page to play again.')
+  }else {
+    alert('try again!');
+  }
 }
 
 
-//TODO: I want to prompt the user to guess the number 12 and get four chances.
-var correctNumber = 12;
-var notDone = true;
-var guess;
 
-for(var guesses = 4; guesses > 0 && notDone; guesses--) {
-guess = prompt('what is my favorite number?');
-if (guess == correctNumber) {
-notDone = false;
-alert('You got it correct!');
+if (numCorrectAnswer < 3) {
+  alert('Thanks for playing ' + userName + ' you got ' + numCorrectAnswer + ' out of 7 correct. it\'s ok, we can still be bffs');
+} else if (numCorrectAnswer >= 3 && numCorrectAnswer < 5 ) {
+  alert('Thanks for playing ' + userName + ' you got ' + numCorrectAnswer + ' out of 7 correct. You know a lot! what a great friend!');
 } else {
-alert('try again!');
-} else if (guesses === 0) {
-alert('sorry, you are out of guesses! Reload page to play again.')
+  alert('Thanks for playing ' + userName + ' you got ' + numCorrectAnswer + ' out of 7 correct. you are my new BFF!');
 }
-}
-}
-user parseInt(guess); //this turns the string into a number
-*/
